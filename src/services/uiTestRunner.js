465bf4bb0449f7ab08048539b2c3c678a5a58afa
K Yuvateja Sainadh <yuvatejasainadh2006@gmail.com>
2026-02-19 18:15:32 +0530

    Update uiTestRunner.js

--- src/services/uiTestRunner.js
+++ src/services/uiTestRunner.js
@@ -1,17 +1,16 @@
 import axios from "axios";
 
+const RUNNER_BASE_URL = process.env.RUNNER_BASE_URL;
+
 export const runUITests = async (baseUrl) => {
   try {
     const response = await axios.post(
-      "http://localhost:5001/runner/run-ui",
+      `${RUNNER_BASE_URL}/runner/run-ui`,
       { baseUrl }
     );
-
     return response.data;
   } catch (error) {
     console.error("UI Runner Error:", error.message);
-    throw new Error(
-      error.response?.data?.message || error.message || "UI runner failed"
-    );
+    throw new Error("UI runner failed");
   }
 };
